8ca57c73e4ecd69ee7fb77a20c84548e01e46e26
lewjunjun072@sina.com <lewjunjun072@sina.com>
2019-01-03 19:25:27 +0800

    取消使用lombok

--- src/main/java/com/microandroid/moudle/emp/dto/Emp.java
+++ src/main/java/com/microandroid/moudle/emp/dto/Emp.java
@@ -1,9 +1,5 @@
 package com.microandroid.moudle.emp.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.util.Date;
 import java.util.List;
 
@@ -11,9 +7,6 @@ import java.util.List;
  * @author LewJun
  * @version v0.1 2018/10/18 19:34 LewJun Exp $$
  */
-@Getter
-@Setter
-@ToString
 public class Emp {
     private Integer empno;
 
@@ -29,4 +22,59 @@ public class Emp {
 
     private List<Emp> empList;
 
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public List<Emp> getEmpList() {
+        return empList;
+    }
+
+    public void setEmpList(List<Emp> empList) {
+        this.empList = empList;
+    }
 }
