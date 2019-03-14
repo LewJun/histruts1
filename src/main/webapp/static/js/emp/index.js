e14e29ca513cb98241e9813eacaf9c89d865d83e
LewJun <lewjun072@sina.com>
2019-03-15 00:27:32 +0800

    jq ajax的全部使用方式

--- src/main/webapp/static/js/emp/index.js
+++ src/main/webapp/static/js/emp/index.js
@@ -2,14 +2,26 @@ $(function () {
     $("a#mockExAjax").click(function () {
         $.ajax({
             url: "empAction.do?method=mockException",
-            success: function (data) {
-                console.log("success: ", data)
+            method: "POST",
+            data: {
+                uname: 'zs',
+                pwd: '123456'
             },
-            error: function (e, data) {
-                console.log("error: ", data)
+            dataType: "text/json",
+            timeout: 5000,
+            async: false,
+            beforeSend: function () {
+                console.log('before send')
+            },
+            success: function (data) {
+                console.log('success');
+                alert("success:" + data)
+            },
+            error: function (e) {
+                alert("error:" + e)
             },
             complete: function (data) {
-                console.log("complete: ", data)
+                console.log('complete')
             }
         })
     });
@@ -17,14 +29,26 @@ $(function () {
     $("a#getEmpListByAjax").click(function () {
         $.ajax({
             url: "empAction.do?method=getEmpList",
+            method: "POST",
+            data: {
+                uname: 'zs',
+                pwd: '123456'
+            },
+            dataType: "text/json",
+            timeout: 5000,
+            async: false,
+            beforeSend: function () {
+                console.log('before send')
+            },
             success: function (data) {
-                console.log("success: ", data)
+                console.log('success');
+                alert("success:" + data)
             },
             error: function (e) {
-                console.error("error: ", e)
+                alert("error:" + e)
             },
             complete: function (data) {
-                console.log("complete: ", data)
+                console.log('complete')
             }
         })
     });
